c451a308910db49544fd2906383b7667620a6df3
Juan <juan.matos@outlook.com>
2021-08-03 18:48:38 -0300

    Resolvido a falha que permitia registro de empréstimo com campos em branco

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -22,17 +22,34 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
         {
-            EmprestimoService emprestimoService = new EmprestimoService();
-            
-            if(viewModel.Emprestimo.Id == 0)
+            if(!string.IsNullOrEmpty(viewModel.Emprestimo.NomeUsuario))
             {
-                emprestimoService.Inserir(viewModel.Emprestimo);
+                EmprestimoService emprestimoService = new EmprestimoService();
+
+                if(viewModel.Emprestimo.Id == 0)
+                {
+                    emprestimoService.Inserir(viewModel.Emprestimo);
+                }
+                else
+                {
+                    emprestimoService.Atualizar(viewModel.Emprestimo);
+                }
+
+                return RedirectToAction("Listagem");
             }
             else
             {
-                emprestimoService.Atualizar(viewModel.Emprestimo);
+                ViewData["mensagem"] = "Por favor, preencha todos os campos";
+
+                LivroService livroService = new LivroService();
+                EmprestimoService emprestimoService = new EmprestimoService();
+
+                CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
+
+                cadModel.Livros = livroService.ListarDisponiveis();
+
+                return View(cadModel);
             }
-            return RedirectToAction("Listagem");
         }
 
         public IActionResult Listagem(string tipoFiltro, string filtro)
